616be1098841242a1f3decf25360267d7bcfe731
Elvis Maki <elvis-maki@hotmail.co.uk>
2025-03-19 10:04:33 +0000

    Implement user registration and login functionality with session management

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
@@ -15,12 +17,21 @@ class RegisteredUserController extends Controller
     public function store()
     {
         // Validate
-        request()->validate([
+        $attributes = request()->validate([
             'first_name'        => ['required'],
             'last_name'         => ['required'],
             'email'             => ['required', 'email', 'max:254'],
-            'password'          => ['required', Password::min(8)->letters()],
+            'password'          => ['required', Password::min(8), 'confirmed'], // Confirming password.
         ]);
+
+        // Create the user
+        $user = User::create($attributes);
+
+        // Log in
+        Auth::login($user);
+
+        // Redirect
+        return redirect('/jobs');
 
 
     }
